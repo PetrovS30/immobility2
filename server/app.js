6e94feb007e2876e01e7537284554698eaa211f5
Ваше имя <@.com>
2025-01-21 12:43:55 +0300

    1) Добавлен uuidv4 для генерации уникальных идентификаторов комнат вместо инкремента count. 2) Добавлена модель пользователя (user) с полями для хранения данных. 3) Создана общая функция removeUserFromRoom для обработки событий chat_leave и disconnect.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
+const { json } = require("stream/consumers");
+const { v4: uuidv4 } = require('uuid');
 
 // Инициализация приложения
 const app = express();
@@ -14,24 +16,24 @@ const io = new Server(server, {
 });
 
 const info = [];
-let count = 0;
-
-
-
 // Обработка подключения клиента
 io.on("connection", (socket) => {
-    let id = 0;
-    let name = '';
-
+    let id = null;
+    const user = {
+        localName: null,
+        ownGender: null,
+        searchPartnerGender: null
+    }
     // Генерация комнаты
     socket.on('createRoom', (data) => {
-        name = data.localName;
-
+        user.localName = data.localName
+        user.ownGender = data.OwnGender
+        user.searchPartnerGender = data.searchOptions.searchPartnerGender
         // Ищем все свободные комнаты
         const findAvailableRoom = () => {
             const find = info
                 .filter(item => item.listUsers.length === 1)
-                .filter(item => item.listUsers.some(user => user.ownGender === data.searchOptions.searchPartnerGender))
+                .filter(item => item.listUsers.some(user => user.ownGender === user.searchPartnerGender))
                 .map(item => item.room);
             if (find.length > 0) {
                 // Рандомно выбираем свободную комнату
@@ -42,11 +44,7 @@ io.on("connection", (socket) => {
                 // Обновляем комнату с новым пользователем
                 info.forEach((item) => {
                     if (item.room === randomRoom) {
-                        item.listUsers.push({
-                            name: data.localName,
-                            ownGender: data.OwnGender,
-                            searchPartnerGender: data.searchOptions.searchPartnerGender,
-                        });
+                        item.listUsers.push(user);
                     }
                 });
                 return true
@@ -55,10 +53,11 @@ io.on("connection", (socket) => {
         }
         const createRoom = (findRoom) => {
             if (!findRoom) {
-                count++;
-                id = count;
+                id = uuidv4();
                 socket.join(id);
-                info.push({ room: count, listUsers: [{ name: data.localName, ownGender: data.OwnGender, searchPartnerGender: data.searchOptions.searchPartnerGender }] })
+                info.push({ room: id, listUsers: [user] })
+                console.log([user]);
+
             }
         }
         createRoom(findAvailableRoom())
@@ -68,20 +67,16 @@ io.on("connection", (socket) => {
         if (roomInfo) {
             io.to(id).emit('roomState', roomInfo.listUsers.length);
         }
-        console.log(info);
-
     });
-
-    socket.on('chat_leave', (localName) => {
-        console.log(info);
-        // Удаление пользователя из комнаты
+    socket.on('sendMessae', (msg) => {
+        io.to(id).emit('liveMsg', { name: user.localName, msg: msg });
+    });
+    const removeUserFromRoom = () => {
         info.forEach((item, index) => {
             if (item.room === id) {
-
-                const indexToRemove = item.listUsers.findIndex(user => user.name === localName);
+                const indexToRemove = item.listUsers.findIndex(user => user.localName === user.localName);
                 if (indexToRemove !== -1) {
                     item.listUsers.splice(indexToRemove, 1);
-
                     io.to(id).emit('currentUser', item.listUsers.length);
                     if (item.listUsers.length === 0) {
                         info.splice(index, 1); // Удаление пустой комнаты
@@ -89,24 +84,9 @@ io.on("connection", (socket) => {
                 }
             }
         });
-    });
-
-    socket.on('sendMessae', (msg) => {
-        io.to(id).emit('liveMsg', { name: name, msg: msg });
-    });
-
-    // socket.on('disconnect', () => {
-    //     info.forEach((item, index) => {
-    //         if (item.room === id) {
-    //             item.listUsers = item.listUsers.filter(user => user.socketId !== socket.id);
-    //             if (item.listUsers.length === 0) {
-    //                 info.splice(index, 1);
-    //             }
-    //         }
-    //     });
-
-    //     io.to(id).emit('currentUser', info.length);
-    // });
+    };
+    socket.on('chat_leave', () => removeUserFromRoom());
+    socket.on('disconnect', () => removeUserFromRoom());
 });
 
 // Запуск сервера
