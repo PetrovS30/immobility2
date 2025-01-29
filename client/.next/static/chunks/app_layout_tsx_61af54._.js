2b1f7766c4834b9c4e47666b7ee4c72caa127ceb
Ваше имя <@.com>
2025-01-29 21:28:12 +0300

    Сохранение изменений перед слиянием

---
+++ client/.next/static/chunks/app_layout_tsx_61af54._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_61af54._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_61af54._.js",
+  "chunks": [
+    "static/chunks/node_modules_872f91._.js",
+    "static/chunks/app_ad4743._.js",
+    "static/chunks/app_globals_scss_271c6b.css"
+  ],
+  "source": "dynamic"
+});
